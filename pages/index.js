00d18b6eabdcef8b481c56abe757f25fa81fc9db
Alexander Graebe <alex.graebe@gmail.com>
2019-07-02 22:57:10 -0700

    First version

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,82 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-import { Button } from 'antd';
-import styled from 'styled-components';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
+import { notification } from 'antd';
+import io from 'socket.io-client';
+import _ from 'lodash';
+import moment from 'moment';
 
-let currentLocation;
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={props.setLocation ? 16 : 8}
-    defaultCenter={props.setLocation || {lat: 37.7933412, lng: -122.3995876} }
-    >
-    {props.setLocation && <Marker position={props.setLocation} />}
-  </GoogleMap>
-));
-
-const StyledButton = styled(Button)`
-    position: fixed;
-    left: 20px;
-    top: 20px;
-    z-index: 9999;
-`;
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-     }
-}
-
-function showPosition(position) {
-    currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
-    console.log(currentLocation);
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: undefined,
+      activity: undefined,
+      health: undefined,
+      summary: undefined,
+      trip: undefined
+    };
   }
 
-const Index = () => (
-        <div>
-            <StyledButton type="primary" shape="circle" icon="compass" onClick={getLocation}/>
+  componentDidMount() {
+    this.socket = io('https://ht-dev-ag.localtunnel.me');
+
+    this.socket.on('location', location => {
+      notification.success({
+        message: `Received new location update`,
+        description: `Device ID: ${location.device_id}`
+      });
+      this.setState({ location });
+    });
+
+    this.socket.on('activity', location => {
+      this.setState({ activity });
+    });
+
+    this.socket.on('health', location => {
+      this.setState({ health });
+    });
+
+    this.socket.on('summary', location => {
+      this.setState({ summary });
+    });
+
+    this.socket.on('trip', location => {
+      this.setState({ trip });
+    });
+  }
+
+  render() {
+    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+      <GoogleMap
+        defaultZoom={(props.coordinates.lat && props.coordinates.lng) ? 14 : 8}
+        defaultCenter={(props.coordinates.lat && props.coordinates.lng) ? props.coordinates : {lat: 37.7933412, lng: -122.3995876} }
+        >
+        {(props.coordinates.lat && props.coordinates.lng) &&
+          <MarkerWithLabel
+            position={props.coordinates}
+            labelAnchor={new google.maps.Point(100, -15)}
+          >
+            <div>{moment(props.label).format('dddd, MMMM Do YYYY, h:mm:ss a')}</div>
+          </MarkerWithLabel>}
+      </GoogleMap>
+    ));
+
+    const coordinates = { lat: _.get(this.state.location, 'data.location.coordinates[0]'), lng: _.get(this.state.location, 'data.location.coordinates[1]') };
+
+    return (
+      <div>
             <MyMapComponent
-                setLocation={currentLocation}
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6V1v4K6l_nm-W9KdCLMObqDgU0znIt-w&v=3.exp&libraries=geometry,drawing,places"
+                coordinates={coordinates}
+                label={_.get(this.state.location, 'recorded_at')}
+                googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyD6V1v4K6l_nm-W9KdCLMObqDgU0znIt-w&v=3.exp&libraries=geometry,drawing,places'
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `100vh` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
             />
         </div>
-);
+    );
+  }
+}
 
 export default Index;
