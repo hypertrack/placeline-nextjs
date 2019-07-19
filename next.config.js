b1288561b84c4091edf5aba5919b2e2ed1db55f3
Alexander Graebe <alex.graebe@gmail.com>
2019-07-19 16:42:13 -0700

    Fix dotenv for heroku

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
+    env: {
+      GMAPS_KEY: process.env.GMAPS_KEY,
+      SERVER_URL: process.env.SERVER_URL
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       // Note: we provide webpack above so you should not `require` it
       // Perform customizations to webpack config
