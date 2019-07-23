7f00385be061f2a174482d83835c2964de2fcd32
Alexander Graebe <alex.graebe@gmail.com>
2019-07-22 18:46:28 -0700

    Update device selection list

--- components/deviceSelection.js
+++ components/deviceSelection.js
@@ -1,6 +1,7 @@
-import { Select } from 'antd';
+import { Select, Avatar, Badge, List } from 'antd';
 import styled from 'styled-components';
 import _ from 'lodash';
+import moment from 'moment';
 import Router from 'next/router';
 
 class DeviceSelection extends React.Component {
@@ -20,12 +21,22 @@ class DeviceSelection extends React.Component {
     const { Option } = Select;
 
     const deviceItems = (_.get(this.props, 'devices', [])).map(item => (
-      <Option key={`select-${item.device_id}`} value={item.device_id}>{_.get(item, 'device_info.name', item.device_id)}</Option>
+      <Option key={`select-${item.device_id}`} value={item.device_id} label={_.get(item, 'device_info.name', item.device_id)}>
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Badge status={(item.device_status === 'disconnected' || item.device_status === 'inactive')? 'error' : 'success'}><Avatar shape="square" src={`../static/status/${item.device_status}.svg`} /></Badge>}
+            title={_.get(item, 'device_info.name', item.device_id)}
+            description={`Last updated at ${moment(item.updatedAt).format("MMMM Do YYYY, h:mm:ss a")}`}
+          />
+        </List.Item>
+      </Option>
     ));
 
     return (
       <StyledSelect
       size='large'
+      placeholder="Select device to see placeline ..."
+      optionLabelProp="label"
       loading={this.props.loading}
       onChange={this.handleChange}>
           {deviceItems}
